e5a28eea0f20eb286d001f2cc4137c97081e83fa
vichik123 <eitan.vichik@gmail.com>
2024-01-13 12:11:27 +0200

    Add Gremlin class

---
+++ Cards/Gremlin.cpp
@@ -0,0 +1,7 @@
+//
+// Created by eitan on 1/13/2024.
+//
+
+#include "Gremlin.h"
+
+Gremlin::Gremlin() : force(5), loot(2), loss(10) {}
---
+++ Cards/Gremlin.h
@@ -0,0 +1,16 @@
+//
+// Created by eitan on 1/13/2024.
+//
+
+#ifndef GREMLIN_H
+#define GREMLIN_H
+
+
+class Gremlin : Card {
+
+public:
+    Gremlin();
+};
+
+
+#endif //GREMLIN_H
